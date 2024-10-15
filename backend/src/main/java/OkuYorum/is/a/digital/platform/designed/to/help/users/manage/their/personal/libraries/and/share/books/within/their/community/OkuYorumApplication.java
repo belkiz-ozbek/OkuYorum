8296a44d3a0ea256c5ad33c7bfcc736ed0fce93c
Belkız Özbek <ceng.belkiz@gmail.com>
2024-10-15 17:01:48 +0300

    Backend ve Frontend klasörleri eklendi.

---
+++ backend/src/main/java/OkuYorum/is/a/digital/platform/designed/to/help/users/manage/their/personal/libraries/and/share/books/within/their/community/OkuYorumApplication.java
@@ -0,0 +1,13 @@
+package OkuYorum.is.a.digital.platform.designed.to.help.users.manage.their.personal.libraries.and.share.books.within.their.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkuYorumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkuYorumApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/OkuYorum/is/a/digital/platform/designed/to/help/users/manage/their/personal/libraries/and/share/books/within/their/community/OkuYorumApplicationTests.java
@@ -0,0 +1,13 @@
+package OkuYorum.is.a.digital.platform.designed.to.help.users.manage.their.personal.libraries.and.share.books.within.their.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkuYorumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
