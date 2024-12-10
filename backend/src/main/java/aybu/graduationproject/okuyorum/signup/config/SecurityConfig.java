0137f1cfe97a17fe76872275775f456a8e22a0a5
muhsin.ozbek <muhsin.ozbek@tubitak.gov.tr>
2024-12-10 23:48:45 +0300

    #15 SecurityFilterChain

--- backend/src/main/java/aybu/graduationproject/okuyorum/signup/config/SecurityConfig.java
+++ backend/src/main/java/aybu/graduationproject/okuyorum/signup/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package aybu.graduationproject.okuyorum.signup.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,7 @@ public class SecurityConfig {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
